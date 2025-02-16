b7b151a7ac96dd34917cffe4d2f1f9d7977d6c17
Ibrahimgarni <ibrahimgarni@outlook.com>
2025-02-17 00:42:47 +0100

    Examen Réussi

--- ExamPythonGarniIbrahim3SI3.py
+++ ExamPythonGarniIbrahim3SI3.py
@@ -31,14 +31,14 @@ def Question_4(resp):
     return None
 
 def Question_5(resp):
-    resp = bytes.fromhex(resp = resp[resp.index("majuscule: ")+len("majuscule: "):]).decode('utf-8')
+    resp = bytes.fromhex(resp[resp.index("majuscule: ")+len("majuscule: "):]).decode('utf-8')
     resp_morse_decoded = []
     for letter in resp.split() :
         resp_morse_decoded.append(morse_dict[letter])
     return ''.join(resp_morse_decoded)
 
 def Question_6(resp):
-    resp = bytes.fromhex(resp = resp[resp.index("majuscule: ")+len("majuscule: "):]).decode('utf-8')
+    resp = bytes.fromhex(resp[resp.index("majuscule: ")+len("majuscule: "):]).decode('utf-8')
     resp_braille_decoded = []
     for letter in resp.split() :
         resp_braille_decoded.append(braille_dict[letter])
@@ -81,13 +81,11 @@ def Question_11(resp):
 
 def Question_12(resp):
     resp = resp[resp.index("message: ")+len("message: "):]
-    for decode_function in (base64.b64decode, base64.b32decode, base64.b16decode, base64.b85decode):
+    for decode_function in (base64.b64decode, base64.b32decode, base64.b16decode, base64.b85decode, base58.b58decode):
         try:
             decoded_resp = decode_function(resp).decode('utf-8')
         except:
             continue
-    print(resp)
-    print(decoded_resp)
     for decalage in range(26):
         resultat = ""
         for caractere in decoded_resp:
@@ -97,7 +95,7 @@ def Question_12(resp):
                 resultat += chr((ord(caractere) - ord('A') + decalage) % 26 + ord('A'))
             else:
                 resultat += caractere
-        for decode_function in (base64.b64decode, base64.b32decode, base64.b16decode, base64.b85decode):
+        for decode_function in (base64.b64decode, base64.b32decode, base64.b16decode, base64.b85decode, base58.b58decode):
             try:
                 return decode_function(resultat).decode('utf-8')
             except:
@@ -123,10 +121,10 @@ braille_dict = {
 #Fonction principale pour lancer le test
 def ExamPython():
     try:
-        reponse =  ["ibrahim/garni/3SI3", "15/02"]
+        reponse =  ["ibrahim/garni/3SI3", "16/02"]
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((host, port))
-        for i in range(11):
+        for i in range(12):
             resp=s.recv(1024)
             resp = resp.decode()
             print(resp)
@@ -148,16 +146,18 @@ def ExamPython():
                 reponse.append(Question_10(reponse))
             elif i == 10:
                 reponse.append(Question_11(resp))
+            elif i == 11:
+                reponse.append(Question_12(resp))
             s.sendall(reponse[i].encode())
         resp = s.recv(1024)
         print(resp.decode())
         return True
     except Exception as e :
         print(f"Echec : {e}")
-        #os.system("clear")
+        os.system("clear")
         s.close()
 
-#fonction principale qui lance le test tant que la question 12 n'est pas atteinte
+#Fonction principale qui lance le test tant que l'examen n'est pas réussi
 def main():
     while not ExamPython():
         time.sleep(2)
